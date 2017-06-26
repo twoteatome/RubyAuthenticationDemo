f84eb725c9130433c6e474ac8049a58d2b856373
twoteatome <viet.phantran1992@gmail.com>
2017-06-26 18:28:57 +0900

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get     "login"    => "session#new"
+  post    "login"    => "session#create"
+  delete  "logout"   => "session#destroy"
+
+  resources :users
+end
